2bca272e10b879cb7198826fe9a686b2746d8543
metaldude <yaroslav-tokar@yandex.ru>
2017-08-30 13:19:56 +0300

    new version

--- app/src/main/java/com/example/metaldude/simplemenu/MainActivity.java
+++ app/src/main/java/com/example/metaldude/simplemenu/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.metaldude.simplemenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("1");
+        menu.add("2");
+        menu.add("3");
+        menu.add("4");
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
